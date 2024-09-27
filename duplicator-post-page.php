e6eba3ed6a1f38b35e793245be82e8b5a833eedf
Iqbal Hossain <iqbal.212015026@green.ac.bd>
2024-09-27 23:11:13 +0600

    Update readme file

--- duplicator-post-page.php
+++ duplicator-post-page.php
@@ -4,7 +4,7 @@
  * Description:       Duplicate post and page with a single click.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            @iqbal1hossain
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -29,7 +29,7 @@ define('DUPLICATOR_POST_PAGE_PLUGIN_DIR', plugin_dir_path(__FILE__));
  * @since 1.0.0
  */
 class Duplicator_Post_Page_Version {
-	const PLUGIN_VERSION = '1.0.1';
+	const PLUGIN_VERSION = '1.0.2';
 
 	public static function get_plugin_version() {
 		return self::PLUGIN_VERSION;
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: duplicate, post, page, meta, taxonomies
 Requires at least: 6.1
 Tested up to: 6.6
 Requires PHP: 7.0
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
