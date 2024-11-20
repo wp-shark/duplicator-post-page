3bc6faefce3f94ad6d0559449ccc1da1cde89f63
Iqbal Hossain <iqbal.212015026@green.ac.bd>
2024-11-20 22:23:20 +0600

    Update

--- duplicator-post-page.php
+++ duplicator-post-page.php
@@ -5,6 +5,8 @@
  * Description:       Duplicate posts and pages with a single click.
  * Requires at least: 6.1
  * Requires PHP:      7.0
+ * Version:           1.0.3
+ * Author:            Iqbal Hossain
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       duplicator-post-page
